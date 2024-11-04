fa6fdee1141e629c09ea50bd58e7da5443454e61
Sherlock Nguyen <quocx2003@gmail.com>
2024-11-04 11:23:51 +0700

    'video5-6-7-8'

--- App.tsx
+++ App.tsx
@@ -1,12 +1,43 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Button, FlatList, ScrollView, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+
+  const [students, setStudents] = useState([
+    { id: 1, name: 'VanQuoc', age: 18 },
+    { id: 2, name: 'LazyBoy', age: 19 },
+    { id: 3, name: 'Hello', age: 20 },
+  ])
+
   return (
     <View style={styles.container}>
-      <Text style={{ color: 'blue', borderColor: "red", borderWidth: 1, fontSize: 30, padding: 4 }}>
-        Hello Guys Iam VQ!
-      </Text>
-      <Text style={styles.hello}>Iam LazyBoy</Text>
+      <Text style={{ fontSize: 50 }}>Hello World</Text>
+
+      <FlatList
+        data={students}
+        keyExtractor={item => item.id + ""}
+        renderItem={(data) => {
+          return (
+            <View key={data.item.id} style={{ padding: 30, backgroundColor: 'pink', marginBottom: 30 }}>
+              <Text>{data.item.name}</Text>
+            </View>
+          )
+        }}
+      >
+
+
+      </FlatList>
+
+      {/* <ScrollView>
+        {students.map(item => {
+          return (
+            <View key={item.id} style={{ padding: 30, backgroundColor: 'pink', marginBottom: 30 }}>
+              <Text>{item.name}</Text>
+            </View>
+          )
+        })}
+      </ScrollView> */}
+
     </View>
   );
 }
@@ -15,12 +46,8 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 15,
+    marginTop: 20
   },
 
-  hello: {
-    fontSize: 20,
-    color: 'green'
-  }
 });
